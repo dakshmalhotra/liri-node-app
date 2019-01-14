98cd3abea0da9f1b9f37ac67c3eb54979379a23d
dakshmalhotra <dakshmalhotra@hotmail.com>
2019-01-14 13:20:25 -0500

    kinda done, nothing i can do to be fully done

---
+++ keys.js
@@ -0,0 +1,14 @@
+/*
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+*/
+
+exports.spotifyKeys = {
+	id: '5cd1d172f98d42cd9296df65e920a800',
+	secret: '13d2cf2f85d04119bdebc354afceef57',
+}
+
